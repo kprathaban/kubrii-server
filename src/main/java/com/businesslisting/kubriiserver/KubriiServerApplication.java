28fa5f95fb8e28491965b6bd912b946c715a3352
Karthikeyan <karthik.revathy@gmail.com>
2022-01-04 00:17:02 +0530

    Adding Kubrii Server

---
+++ src/main/java/com/businesslisting/kubriiserver/KubriiServerApplication.java
@@ -0,0 +1,16 @@
+package com.businesslisting.kubriiserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class KubriiServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KubriiServerApplication.class, args);
+		System.out.println("Eureka Server started.");
+	}
+
+}
---
+++ src/test/java/com/businesslisting/kubriiserver/KubriiServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.businesslisting.kubriiserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KubriiServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
